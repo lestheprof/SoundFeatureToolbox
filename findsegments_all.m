ce533b0a0ce9b397f3482a91cf9b7db49c889c1d
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2018-10-24 17:04:45 +0100

    findsegments_all now working

--- findsegments_all.m
+++ findsegments_all.m
@@ -3,9 +3,16 @@ function [nFiles] = findsegments_all(soundDirectory,soundFileList, outputDirecto
 %soundDirectory named in soundFileList
 %   calls findsegments_1.m repeatedly to create the segments. places the
 %   output segments in outputDirectory/<filename>.segs one for each file
+%
+% LSS 24 OCTOBER 2018 
 
 %parameters for calls to findsegments_1.m default to below values,
 %overwritable using varargin. 
+sigma1 = 0.02 ;
+sigmaratio = 1.2 ;
+fs = 44100 ; % adjust if required
+dtperelement = 1.0/fs ;
+nsamples = 4000 ;
 % default values for BPF etc: use varargin to adjust
 minCochFreq = 100 ; % BPF parameters
 maxCochFreq = 5000 ;
@@ -16,13 +23,21 @@ smoothlength = 0.01 ; % Bartlett filter parameter length of triangular (bartlett
 % constants for segmentation from oo signal
 threshold = 0.04 ;
 G_quiet = 0.05 ;
-k_minimin = 0.4 ; % not used just yet...
+K_minmin = 0.4 ; % not used just yet...
 segStartAdjust = 0.05 ; % allows adjusting back the time of a segment start to ZX before peak
 
 i = 1 ;
 while(i<=size(varargin,2))
     switch lower(varargin{i})
-        
+        case 'sigma1'
+            sigma1 = varargin{i+1};
+            i=i+1 ;
+        case 'sigmaratio'
+            sigmaratio = varargin{i+1};
+            i=i+1 ;
+        case 'nsamples'
+            nsamples = varargin{i+1};
+            i=i+1 ;
         case 'mincochfreq'
             minCochFreq = varargin{i+1};
             i=i+1 ;
@@ -60,19 +75,31 @@ end %while
 inputfid = fopen(soundFileList) ;
 fline = fgetl(inputfid) ;
 nooffiles = 1 ;
-while ischar(fline)
+while (ischar(fline) && (length(fline) > 0))
     filelist{nooffiles} = fline ;
     fline = fgetl(inputfid) ;
     nooffiles = nooffiles + 1 ;
 end
 nooffiles = nooffiles - 1 ;
+fclose(inputfid) ;
 
+% create output directory if required
+if (~(exist(outputDirectory, 'dir') == 7))
+    % create folder
+    mkdir(outputDirectory) ;
+end
 % process each file, one by one
 for i = 1:nooffiles
    filenameroot =  strsplit(filelist{i}, '.') ;
-   [segments] = findsegments_1([soundDirectory '/' filelist{i} ],sigma1, sigmaratio, dtperelement, nsamples, varargin) ;
-   save([outputDirectory '/' filenameroot '.segs'], 'segments') ;
+   % call below needs all varargin parameters set
+   [segments] = findsegments_1([soundDirectory '/' filelist{i} ],sigma1, sigmaratio, dtperelement, nsamples,...
+       'mincochfreq',minCochFreq, 'maxcochfreq', maxCochFreq, 'n_erbs', N_erbs, 'nfilt',  nFilt, ...
+       'smoothlength', smoothlength, 'threshold', threshold , 'g_quiet', G_quiet, 'k_minmin', K_minmin, ...
+       'segstartadjust',  segStartAdjust) ;
+   save([outputDirectory '/' filenameroot{1} '_segs.mat'], 'segments') ;
 end % for
+
+nFiles = nooffiles ; % return number of files processed
 
 end % function
 
