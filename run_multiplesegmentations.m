0f14af9842b42b945f3a1b8e7af9dcccf7c5b4ff
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2019-03-31 16:11:08 +0100

    minor fixes

--- run_multiplesegmentations.m
+++ run_multiplesegmentations.m
@@ -1,14 +1,28 @@
 function segments = run_multiplesegmentations(sd, filelist, nfilters)
-% run_multiplesegmentations:
+% run_multiplesegmentations: LSS March 2019 
+% l.s.smith@cs.stir.ac.uk
+%
 % function to run through segmenting a single file (1st in the list at
 % [sd  / filelist] with vaying number of filters 1 to nfilters.
 % creates a lot of files with the segmentations produced, and then loads
-% them into  a single structure segments which is nfilters by 1 ,l with the
-% segmentation foir each number of filters.
+% them into  a single structure segments which is nfilters by 1 with the
+% segmentation for each number of filters. The intent it that the user
+% compare what they expect with what's produced, for example for continuous
+% sopeech and continuoius speech in noiose. Note that continuous speech
+% does not have word boundaries (for example, the word "semiquaver" is pronounced in
+% exactly the same way as "semi quaver" would be in continuous speech). What
+% this
+% system finds is onsets and offsets, and so it is sensitive to starts and
+% finishes of eneregy in different parts of the spectrum.
 %
-% sd is the directory wioth the sounds (.wav, for example)
+% sd is the directory with the sounds (.wav, for example) (so
+% 'stimuli_2019' might be appropriate)
 % filelist is the file containing the name of the file to be processed
-% nfilters is the number of filters to be used
+% e.g. 'fournmubers.wav'
+% nfilters is the number of filters to be used (e.g. 25)
+%
+% returns a structure array with nfilters elements, each containing the
+% segmentation prxoduced for that  number of channels.
 %
 % intended simply to show effect of filter number
 
