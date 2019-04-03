5bec35a4d95e0236fe523ade818a19fc452f89b6
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2019-04-03 13:12:07 +0100

    starting NN based segmentation technique

--- iandfneurons.m
+++ iandfneurons.m
@@ -13,18 +13,28 @@ function spiketimes = iandfneurons(activity, fs, th, diss, rp, rrp)
 % needs thought: requires capability for a RP, possible an RRP
 % and better to have output take the form of an array suitable for spikeraster
 % sort out optional parameters
-if nargin < 3 th = 1; end ;
-if nargin < 4 diss = 100; end ;
-if nargin < 5 rp = 0.004; end ; % in seconds
-if nargin < 6 rrp = 0 ; end ;
+%
+% tidied up  a bit LSS 2 April 2019.
+if nargin < 3 
+    th = 1; 
+end 
+if nargin < 4 
+    diss = 100;
+end 
+if nargin < 5 
+    rp = 0.004; 
+end  % in seconds
+if nargin < 6 
+    rrp = 0 ; 
+end 
 
 maxonsetspikes = 100000 ; % adjust if required
 
 [numNeurons, datalength] = size(activity) ;
 deltaT = 1.0 / fs ;
-thresholdVector = th * ones(1, numNeurons) ;
+% thresholdVector = th * ones(1, numNeurons) ;
 onsetActivity = zeros(1,numNeurons) ;
-zeroActivity = zeros(1, numNeurons) ;
+% zeroActivity = zeros(1, numNeurons) ;
 rpvector = zeros(1, numNeurons) ;
 spiketimes = zeros([maxonsetspikes 2])  ; % preallocate
 nspikes = 0 ;
@@ -36,19 +46,20 @@ for tau=1:datalength
    % disp(rpvector) ;
    onsetActivity = onsetActivity + deltaT .* (activity(:,tau)' ...
       - diss .* onsetActivity) ;
-   % check if any are  nonnegative and fix
-   negact = onsetActivity < zeroActivity ;
-   neglist = find(negact) ;
-   for i=1:length(neglist) 
-      onsetActivity(neglist(i)) = 0 ;
-   end ;
+   % check if any are  negative and fix
+%    negact = onsetActivity < zeroActivity ;
+%    neglist = find(negact) ;
+%    for i=1:length(neglist) 
+%       onsetActivity(neglist(i)) = 0 ;
+%    end 
+    onsetActivity = max(0, onsetActivity) ; % clearer!
    % decrement refractory period vector
    if any(rpvector) % are we in any ref period?
       rpentries = find(rpvector) ;
       for j=1:length(rpentries)
          rpvector(rpentries(j)) = rpvector(rpentries(j))- 1 ;
-      end ;
-   end ;
+      end 
+   end 
    % does it fire ?
    % first adjust thresholds of those in rrp
    % not yet implemented
@@ -63,15 +74,15 @@ for tau=1:datalength
          if rpvector(firelist(k)) == 0
              nspikes = nspikes + 1 ;
              if (nspikes > maxonsetspikes)
-                 error(['iandfneurons: too many onset spikes: adjust maxonsetspikes']) ;
+                 error('iandfneurons: too many onset spikes: adjust maxonsetspikes') ;
              end
             % update spike list
             spiketimes(nspikes,:) = [firelist(k) tau/fs] ;% this should be faster! alter this: slow
             % update refractory period
             rpvector(firelist(k)) = rpsamples ;
-         end ;
-      end ;
-   end ;
-end ;
+         end 
+      end 
+   end 
+end 
 spiketimes = spiketimes(1:nspikes, :) ; % contract to size actually used
 end
