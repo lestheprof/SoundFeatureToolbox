170b4a01f42cd13e4c2fc2bc403d5cb873548790
Leslie S. Smith <l.s.smith@cs.stir.ac.uk>
2019-04-12 13:35:19 +0100

    Primarilty new and better documntation

--- run_multiplesegmentations_2.m
+++ run_multiplesegmentations_2.m
@@ -1,4 +1,4 @@
-function segments = run_multiplesegmentations_2(sd, filelist, nfilters)
+function segments = run_multiplesegmentations_2(sd, filelist, nfilters, varargin)
 % run_multiplesegmentations_2: LSS March 2019 
 % l.s.smith@cs.stir.ac.uk
 %
@@ -25,10 +25,25 @@ function segments = run_multiplesegmentations_2(sd, filelist, nfilters)
 % segmentation prxoduced for that  number of channels.
 %
 % intended simply to show effect of filter number
+startfilter = 1 ;
 
-for nf = 1:nfilters
+i = 1 ;
+while(i<=size(varargin,2))
+    switch lower(varargin{i})
+        case 'startfilter'
+            startfilter = varargin{i+1};
+            i=i+1 ;
+            otherwise
+            error('run_multiplesegmentations_2: Unknown argument %s given',varargin{i});
+    end
+    i=i+1 ;
+end
+            
+for nf = startfilter:nfilters
+    convergence = floor(nf/6) ;
+    % convergence = 0 ;
     s1 = findsegments_all_2(sd, [sd '/' filelist], sd, 'nfilt', nf, 'filesuffix', num2str(nf), 'minseglength', 0.1, 'sigmaratio', 1.1 ,...
-        'onset_wt' ,150, 'offset_wt', 150, 'convergence', 6, 'logonset', true) ;
+        'onset_wt' ,500, 'offset_wt', 500, 'convergence', convergence, 'logonset', true) ;
 end
 if ~(s1 == 1)
     disp(['This version expects only one file to be processed: number of files = ' num2str(s1) ' only 1st file processed']) ;
@@ -47,7 +62,7 @@ fclose(inputfid) ;
 
 filenameroot =  strsplit(filenames{1}, '.') ;
 
-for i =1:nfilters
+for i = startfilter:nfilters
     segments(i,:) = load([sd '/' filenameroot{1} num2str(i) '_segs.mat'], 'segments') ;
     % delete the files
     delete([sd '/' filenameroot{1} num2str(i) '_segs.mat']);
